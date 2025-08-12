7b37654eb9e5827b2dad360135f586db196ce5dd
Daniel Pareja Londoño <dpareja1394@gmail.com>
2025-08-11 21:11:39 -0500

    Avance creación domain y repository

---
+++ 2025_2/Servicios/cinema/src/main/java/co/edu/usbcali/cinema/domain/PurchaseStatus.java
@@ -0,0 +1,25 @@
+package co.edu.usbcali.cinema.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "purchase_status")
+public class PurchaseStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 20, unique = true)
+    private String name;
+
+}
