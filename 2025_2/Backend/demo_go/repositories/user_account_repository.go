8073e791e46b391b5863b87c9add4113c66baed7
Daniel Pareja Londoño <dpareja1394@gmail.com>
2025-10-22 20:30:21 -0500

    Add UserAccountRepository interface and implementation

--- 2025_2/Backend/demo_go/repositories/user_account_repository.go
+++ 2025_2/Backend/demo_go/repositories/user_account_repository.go
@@ -1,1 +1,15 @@
 package repositories
+
+import (
+  "gorm.io/gorm"
+  "demo_go/domains"
+)
+
+type UserAccountRepository interface {
+  GetUserClientAccounts() ([]domains.UserAccount, error)
+}
+
+type userAccountRepositoryImpl struct {
+  db *gorm.DB
+}
+
