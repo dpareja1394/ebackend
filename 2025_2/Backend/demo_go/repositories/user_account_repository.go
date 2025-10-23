d9b1b91ae2da462039094f38fb51c73b000e5e8d
Daniel Pareja Londoño <dpareja1394@gmail.com>
2025-10-22 20:48:06 -0500

    Add user account repository and retrieval method

--- 2025_2/Backend/demo_go/repositories/user_account_repository.go
+++ 2025_2/Backend/demo_go/repositories/user_account_repository.go
@@ -13,3 +13,19 @@ type userAccountRepositoryImpl struct {
   db *gorm.DB
 }
 
+func NewUserAccountRepository(db *gorm.DB) UserAccountRepository {
+  return userAccountRepositoryImpl{db: db}
+}
+
+func (u userAccountRepositoryImpl) GetUserClientAccounts() ([]domains.UserAccount, error) {
+  var userAccounts []domains.UserAccount
+  query := u.db.Table("user_account").Order("id ASC")
+
+  err := query.Find(&userAccounts).Error
+  
+  if err != nil {
+    return nil, err
+  }
+  return userAccounts, nil
+}
+
