8364256f8848ffe62aee11eab0d8c6dfc77eb9a2
Daniel Pareja Londoño <dpareja1394@gmail.com>
2026-02-23 21:04:10 -0500

    Agregar Clase - Entidad DocumentType

--- 2026_1/Servicios/Proyecto/ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/model/DocumentType.java
+++ 2026_1/Servicios/Proyecto/ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/model/DocumentType.java
@@ -1,19 +1,28 @@
 package co.edu.usbcali.ecommerceusb.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
-import java.sql.Timestamp;
+import java.time.OffsetDateTime;
 
+@Entity
+@Table(name = "document_types")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class DocumentType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "code", nullable = false, unique = true)
     private String code;
+
+    @Column(name = "name", nullable = false)
     private String name;
-    private Timestamp createdAt;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
 }
