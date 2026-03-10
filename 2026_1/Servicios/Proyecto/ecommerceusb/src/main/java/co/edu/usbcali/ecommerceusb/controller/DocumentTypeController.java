9112c22bcdd32c44a61e4195d61dd7db92aa4c62
Daniel Pareja Londoño <dpareja1394@gmail.com>
2026-03-09 21:05:32 -0500

    Creacion controller

--- 2026_1/Servicios/Proyecto/ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/controller/DocumentTypeController.java
+++ 2026_1/Servicios/Proyecto/ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/controller/DocumentTypeController.java
@@ -1,4 +1,15 @@
 package co.edu.usbcali.ecommerceusb.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DocumentTypeController {
+
+    //Inyección de dependencias por constructor
+
+    // Inyección de dependencias por Autowired
+
+    // Diferencias entre cada uno y cuál es más óptimo
+
+    // Capa Service para qué sirve en SpringBoot
 }
