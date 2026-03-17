c404f77c5a113675b1160a28d45703f01175f118
Daniel Pareja Londoño <dpareja1394@gmail.com>
2026-03-16 21:08:16 -0500

    Controller

--- 2026_1/Servicios/Proyecto/ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/controller/DocumentTypeController.java
+++ 2026_1/Servicios/Proyecto/ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/controller/DocumentTypeController.java
@@ -1,15 +1,34 @@
 package co.edu.usbcali.ecommerceusb.controller;
 
+import co.edu.usbcali.ecommerceusb.model.DocumentType;
+import co.edu.usbcali.ecommerceusb.repository.DocumentTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/document-type")
 public class DocumentTypeController {
 
-    //Inyección de dependencias por constructor
+    @Autowired
+    private DocumentTypeRepository documentTypeRepository;
 
-    // Inyección de dependencias por Autowired
+    @GetMapping("/all")
+    public List<DocumentType> getAll() {
+        return documentTypeRepository.findAll();
+    }
 
-    // Diferencias entre cada uno y cuál es más óptimo
-
-    // Capa Service para qué sirve en SpringBoot
+    @GetMapping("/{id}")
+    public ResponseEntity<DocumentType> getById(@PathVariable Integer id) {
+        return new ResponseEntity<>(
+                documentTypeRepository.getReferenceById(id),
+                HttpStatus.OK
+        );
+    }
 }
