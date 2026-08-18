3d7356f73677aae8df5dd94e36ee9db9d2e97094
Daniel Pareja Londoño <dpareja1394@gmail.com>
2026-08-18 18:11:59 -0500

    Microservicio dnetwork

---
+++ 2026_2/Backend/microservicios/dnetwork-java/src/main/java/co/edu/usbcali/dnetwork_java/DnetworkJavaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usbcali.dnetwork_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnetworkJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnetworkJavaApplication.class, args);
+	}
+
+}
