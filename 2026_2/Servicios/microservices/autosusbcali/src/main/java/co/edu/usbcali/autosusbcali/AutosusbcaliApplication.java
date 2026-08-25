787fcf84005c728d0a5e12d15c5b450d1562640f
Daniel Pareja Londoño <dpareja1394@gmail.com>
2026-08-25 09:50:56 -0500

    Primer commit Servicios Back 2026-2

---
+++ 2026_2/Servicios/microservices/autosusbcali/src/main/java/co/edu/usbcali/autosusbcali/AutosusbcaliApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usbcali.autosusbcali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosusbcaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutosusbcaliApplication.class, args);
+	}
+
+}
---
+++ 2026_2/Servicios/microservices/autosusbcali/src/test/java/co/edu/usbcali/autosusbcali/AutosusbcaliApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usbcali.autosusbcali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosusbcaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
